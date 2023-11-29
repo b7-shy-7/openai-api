bb94bbb34357585dbaf9f788af93aa7f5d077b2e
PC <2368458361@qq.com>
2023-11-29 18:51:56 +0800

    尝试使用api使用gpt

---
+++ api.py
@@ -0,0 +1,39 @@
+import os
+import openai
+from openai import OpenAI
+
+# openai.api_key = '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'
+
+# from openai import OpenAI
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI()
+history = ""
+while(1):
+    message = input()
+    if message.lower() == 'stop':
+        break
+    # message = "1+1=?"
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        # response_format={ 'type': "json_object"},
+        messages=[
+            # {"role": "system", "content": "you are a hospitable and knowledgable helper"},
+            # {"role": "user", "content": message},
+            {"role": "assistant", "content": history},
+            {"role": "user", "content": message}
+        ],
+        # seed = 1000000
+        stream = True,
+    )
+    ans = ""
+    for chunk in completion:
+        print(chunk.choices[0].delta.content, end ='')
+        try:
+            ans += chunk.choices[0].delta.content
+        except:
+            pass
+    # history += message
+    history += (message + ans + ' ')
+    print()
+    # print(completion.choices[0].message.content)
